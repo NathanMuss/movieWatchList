8caa1f7c83d8a849b481052484660df7c4b758c7
NathanMuss <nathanmuss@gmail.com>
2019-10-03 14:01:10 -0600

    Notes + Android package name.

--- src/api/omdb.js
+++ src/api/omdb.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
+  // Yes, I know my API keys are publicly stored here.  I wanted to make this a complete, working repo that is easily run when cloned.
+  // Please do not abuse my poor little API keys...
   baseURL: "http://www.omdbapi.com/?apikey=846fe7b4&"
 });
